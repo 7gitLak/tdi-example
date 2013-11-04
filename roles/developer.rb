5bab9ccf5060dee0f5cdcabb5fc8a6633db94f72
Dirk Michael Goldschmidt <dirk-michael.goldschmidt@online.de>
2013-11-04 22:06:08 +0100

    develober role chagned to include chruby

--- roles/developer.rb
+++ roles/developer.rb
@@ -1,5 +1,17 @@
 name "developer"
 description "for developer machine"
-run_list (
-	"recipe[irc]"
+run_list(
+	"recipe[irc]",
+	"recipe[chruby::system]"
+)
+
+
+default_attributes(
+	"chruby" => {
+		"rubies" => {
+			"1.9.3-p392" => false,
+			"1.9.3-p429" => true
+			},
+		"default" => "1.9.3-p429"
+		}
 )
