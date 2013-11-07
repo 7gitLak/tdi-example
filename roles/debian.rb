bf5b24960566d27870e5ae80336c8096a5d64254
Dirk Michael Goldschmidt <dirk-michael.goldschmidt@online.de>
2013-11-07 20:16:36 +0100

    vagrant added, changed for vagrant setup

---
+++ roles/debian.rb
@@ -0,0 +1,8 @@
+name "debian"
+
+default_attributes(
+	"vagrant" => {
+		"url" => "http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/vagrant_1.3.5_i686.deb",
+		"checksum" => "30a656ba96b07618e3b2506d3bc3ded706a33f375a46d866504c567484820137"
+	}
+)
--- roles/developer.rb
+++ roles/developer.rb
@@ -1,18 +1,5 @@
 name "developer"
 description "for developer machine"
 run_list(
-	"recipe[irc]",
-	"recipe[chruby::system]",
-	"recipe[virtualbox]"
-)
-
-
-default_attributes(
-	"chruby" => {
-		"rubies" => {
-			"1.9.3-p392" => false,
-			"1.9.3-p429" => true
-			},
-		"default" => "1.9.3-p429"
-		}
+	"recipe[vagrant]"
 )
